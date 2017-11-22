d734ccdf47def862819281e643b53b315240040e
Nuša Pukšič <npuksic@imt.si>
2017-11-22 13:20:40 +0100

    languages

---
+++ js/nusapuksic.js
@@ -0,0 +1,41 @@
+﻿$("#sec-3").hide();
+$("#sec-4").hide();
+
+function infoFillEn() {
+  $("#intro").text("Welcome.");
+  $("#who").text("Who am I.");
+  $("#phd").text("// PhD in nanosciences and nanotechnologies");
+  $("#values").text("// I value happiness and curiosity.");
+  $("#what").text("What I do.");
+  $("#science").text("Science");
+  $("#latest").text("// Latest "); 
+  $("#extra").text("Extra");
+  $("#spanish").text("Learning Spanish");
+  $("#showcase h2").text("Showcase.");
+}
+
+$("#en").click(function() {
+ infoFillEn();
+});
+
+
+$("#sl").click(function() {
+  $("#intro").text("Dobrodošli.");
+  $("#who").text("Kdo sem.");
+  $("#phd").text("// Dr nanoznanosti in nanotehnologij");
+  $("#values").text("// Moji vrednosti sta sreča in radovednost.");
+  $("#what").text("Moje delo.");
+  $("#science").text("Znanost");
+  $("#latest").text("// Zadnje "); 
+  $("#extra").text("Dodatno");
+  $("#spanish").text("Učim se španščino");
+  $("#showcase h2").text("Razglednica.");
+});
+
+$( document ).ready(function() {
+  setTimeout(function() {
+    infoFillEn();
+    $("#sec-3").fadeIn( "slow");
+    $("#sec-4").fadeIn( "slow");
+  }, 2000)
+});
