1ebd13c4654a6cf6c1e3c027bfa315f8fc092d5a
Nuša Pukšič <npuksic@imt.si>
2018-03-21 10:50:40 +0100

    cleanup

--- js/nusapuksic.js
+++ js/nusapuksic.js
@@ -2,12 +2,9 @@
   $("#intro").text("Welcome.");
   $("#who").text("Who am I.");
   $("#phd").text("// PhD in nanosciences and nanotechnologies");
-  $("#values").text("// I value happiness and curiosity.");
   $("#what").text("What I do.");
   $("#science").text("Science");
   $("#latest").text("// Latest "); 
-  $("#extra").text("Extra");
-  $("#spanish").text("Learning Spanish");
   $("#showcase h2").text("Showcase.");
 }
 
@@ -20,12 +17,9 @@ $("#sl").click(function() {
   $("#intro").text("Dobrodošli.");
   $("#who").text("Kdo sem.");
   $("#phd").text("// Dr nanoznanosti in nanotehnologij");
-  $("#values").text("// Moji vrednosti sta sreča in radovednost.");
   $("#what").text("Moje delo.");
   $("#science").text("Znanost");
   $("#latest").text("// Zadnje "); 
-  $("#extra").text("Dodatno");
-  $("#spanish").text("Učim se španščino");
   $("#showcase h2").text("Razglednica.");
 });
 
